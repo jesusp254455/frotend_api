8a1d5d2ec5554e6206a392491519e0d675f46696
jesusp254455 <jpenaranda633gmail.com>
2023-02-27 13:16:26 -0500

    jp

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
     const registrar = async (e)=>{
       e.preventDefault();
-       await axios.post(url, {
+       const a = await axios.post(url, {
         nombre: nombre,
         apellido: apellido,
         tipo_doc: tipo,
@@ -48,7 +48,7 @@ function App() {
         telefono: telefono,
         correo: correo
       })
-
+      console.log(a);
       mostrar()
     }
 
@@ -71,19 +71,19 @@ function App() {
       <div class="mb-3 row mt-3">
     <label for="inputNombre" class="col-sm-2 col-form-label">Nombre</label>
     <div class="col-sm-10">
-      <input onChange={(e)=> setnombre(e.target.value)} type="text" class="form-control" id="Nombre"/>
+      <input onChange={(e) => setnombre(e.target.value)} type="text" class="form-control" id="Nombre"/>
     </div>
     </div>
     <div class="mb-3 row mt-3">
     <label for="inputApellido" class="col-sm-2 col-form-label">Apellido</label>
     <div class="col-sm-10">
-      <input onChange={(e)=>setapellido(e.target.value)} type="text" class="form-control" id="Apellido"/>
+      <input    onChange={(e) => setapellido(e.target.value)} type="text" class="form-control" id="Apellido"/>
     </div>
     </div>
     <div class="mb-3 row ml-5">
     <label for="inputNombre" class="col-sm-2 col-form-label">Eliga tipo</label>
     <div class="col-sm-8">
-    <select class="form-select ml-5" aria-label="Default select example">
+    <select onChange={(e) => settipo(e.target.value)} class="form-select ml-5" aria-label="Default select example">
   <option selected>Documento</option>
   <option value="1">CC</option>
   <option value="2">TI</option>
@@ -93,19 +93,19 @@ function App() {
     <div class="mb-3 row mt-3">
     <label for="inputnum_doc" class="col-sm-2 col-form-label">Nº Doc</label>
     <div class="col-sm-10">
-      <input type="number" class="form-control" id="num_doc"/>
+      <input onChange={(e) => setnumero(e.target.value)} type="number" class="form-control" id="num_doc"/>
     </div>
     </div>
     <div class="mb-3 row mt-3">
     <label for="inputtelefono" class="col-sm-2 col-form-label">Telefono</label>
     <div class="col-sm-10">
-      <input type="number" class="form-control" id="telefonoc"/>
+      <input onChange={(e) =>settelefono(e.target.value)} type="number" class="form-control" id="telefonoc"/>
     </div>
     </div>
     <div class="mb-3 row mt-3">
     <label for="input_email" class="col-sm-2 col-form-label">Email</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="email"/>
+      <input type="email" onChange={(e) => setcorreo(e.target.value)} class="form-control" id="email"/>
     </div>
     </div>
   <div class="modal-footer">
